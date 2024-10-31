39d76e9b397cc2da338d920a55134319ab6b268c
Eryk <eryk.tatol@stud.techin.lt>
2024-10-31 14:01:28 +0400

    finalized

---
+++ script.js
@@ -0,0 +1,34 @@
+// const numbers = [1, 2, 3, 4];
+// const sumArray = (arr) => {
+//   let sumOfArray = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sumOfArray += arr[i];
+//   }
+//   return sumOfArray;
+// };
+// console.log(sumArray(numbers));
+
+// const numbers = [2, 3, 4];
+// const doubleArray = (arr) =>{
+//    return numbers.map(num => num*2)
+// }
+// console.log(doubleArray(numbers));
+
+// const numbers = [1, 2, 3, 4, 5];
+// const sortArray = (arr) => {
+//     const filtered = arr.filter(num => num % 2 === 0)
+// return filtered}
+// console.log(sortArray(numbers));
+
+// const arrayFruits = ["banana", "apple", "cherry"]
+// const sortArray = (arr) =>{
+// const sorted = arr.sort()
+// return sorted}
+// console.log(sortArray(arrayFruits));
+
+// const array = ["hello", "world"];
+// const capitalizeWords = (arr) => {
+//   const capitalized = arr.map(word => word.charAt(0).toUpperCase()+word.substring(1));
+//   return capitalized;
+// };
+// console.log(capitalizeWords(array));
